032c4856c3e3249320b46a9dd80dde79c30140bd
eduardobagues <eduardobaguespardo@gmail.com>
2016-11-30 11:46:52 +0100

    Update Ejercicio08.java

--- Ejercicio08/src/Ejercicio08.java
+++ Ejercicio08/src/Ejercicio08.java
@@ -4,36 +4,55 @@ public class Ejercicio08 {
 	public static void main(String[] args) {
 		
 		Scanner teclado = new Scanner(System.in);
+	
 		
-		String frase, codificada = "";
+		String frase, cod="", dec="";
+		int num;
 		
-		int num, num_ascii;
-		
-		
-		
-		System.out.println("Mete una frase");
+		System.out.println("Frase");
 		frase = teclado.nextLine();
-		
 		frase = frase.toUpperCase();
 		
-		System.out.println("Introduce un numero");
+		System.out.println("Numero desplazamiento");
 		num = teclado.nextInt();
-
-		for(int cont = 0; cont<frase.length(); cont++)
-			codificada += (char)(frase.charAt(cont) + num);
-		System.out.println("la frase codificada es "+codificada);
 		
-		System.out.println("La frase decodificada es ");
-		for(int cont = 0; cont<frase.length(); cont++)
-			System.out.print(frase.charAt(cont));
+		System.out.println("\nOriginal: "+frase);
 		
-	
+		
+		for(int cont = 0; cont<frase.length(); cont++ ){
+			
+			
+			if((frase.charAt(cont)+num) > 90){
+				
+				cod +=  (char)((frase.charAt(cont)+num)-26);
+				
+			}
+			
+			else{ cod += (char)(frase.charAt(cont)+num);}
+			
 			
 		}
-
 		
+		System.out.println("\nCodificada: "+cod);
+		
+		for(int cont1 = 0; cont1<cod.length(); cont1++ ){
+			
+			
+			if((cod.charAt(cont1)-num) < 65){
+				
+				dec += (char)((cod.charAt(cont1)-num)+26);
+				
+			}
+			
+			else{dec += (char)(cod.charAt(cont1)-num);}
+			
+			
+		}
+		
+		System.out.println("\nDecodif: "+dec);
+		
+			
+		}
 	
-		
-		
 	}
 
